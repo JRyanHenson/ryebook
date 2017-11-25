5ca0cc64d64ca09f801a363ebed15a82aa241760
JRyanHenson <hensojr@gmail.com>
2017-11-24 21:46:49 -0700

    Fixed urls.py file location.

--- ryebook/ryebook/urls.py
+++ ryebook/ryebook/urls.py
@@ -1,42 +1,16 @@
-"""ryebook URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
-from django.conf.urls import url
-from . import views
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    # url(r'^login/$', views.user_login, name='login'),
-    url(r'^$', views.dashboard, name='dashboard'),
-
-    url(r'^register/$', views.register, name='register'),
-    url(r'^edit/$', views.edit, name='edit'),
-
-    # login / logout urls
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
-    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
-
-    # change password urls
-    url(r'^password-change/$', 'django.contrib.auth.views.password_change', name='password_change'),
-    url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
-
-    # restore password urls
-    url(r'^password-reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
-    url(r'^password-reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
-    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
-    url(r'^password-reset/complete/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^account/', include('account.urls')),
+    # python-social-auth
+    url('social-auth/', include('social.apps.django_app.urls', namespace='social')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
